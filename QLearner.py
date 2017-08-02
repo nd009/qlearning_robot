6e50114f46e5dfaf7167f5319ac7ab01f04ed92c
jaimezwalker <jaimezwalker@gmail.com>
2017-08-02 17:40:53 +0800

    initial commit

---
+++ QLearner.py
@@ -0,0 +1,48 @@
+"""
+Code forked from Tucker Balch ML4T course
+Template for implementing QLearner  
+"""
+
+import numpy as np
+import random as rand
+
+class QLearner(object):
+
+    def __init__(self, \
+        num_states=100, \
+        num_actions = 4, \
+        alpha = 0.2, \
+        gamma = 0.9, \
+        rar = 0.5, \
+        radr = 0.99, \
+        verbose = False):
+
+        self.verbose = verbose
+        self.num_actions = num_actions
+        self.s = 0
+        self.a = 0
+
+    def querysetstate(self, s):
+        """
+        @summary: Update the state without updating the Q-table
+        @param s: The new state
+        @returns: The selected action
+        """
+        self.s = s
+        action = rand.randint(0, self.num_actions-1)
+        if self.verbose: print("s =", s,"a =",action)
+        return action
+
+    def query(self,s_prime,r):
+        """
+        @summary: Update the Q table and return an action
+        @param s_prime: The new state
+        @param r: The ne state
+        @returns: The selected action
+        """
+        action = rand.randint(0, self.num_actions-1)
+        if self.verbose: print("s =", s_prime,"a =",action,"r =",r)
+        return action
+
+if __name__=="__main__":
+    print("This is QLearner Class")
